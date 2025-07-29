3cec1c4471c786b98622610ccbbd462ce507f75f
oussama <ademusmh166@gmail.com>
2025-07-30 00:32:23 +0100

    modified backend

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,16 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
+//midelware
+app.use(express.json());
+app.use(cors());
+
+// API
+
+app.get("/",(req,res)=>{
+    res.send("API working");
+})
+
+app.listen(port,()=>{
+    console.log("Port its working");
+})
