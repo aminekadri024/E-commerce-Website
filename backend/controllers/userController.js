5928851a7b463b11680242723009f9883c12d0a7
oussama <ademusmh166@gmail.com>
2025-08-05 19:33:58 +0100

    modified backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
-import userModel from "../models/userModel.js"
+import validator from "validator";
+import bcrypt from "bcrypt";
+import userModel from "../models/userModel.js";
 
 //route for user login
 const loginUser = async (req, res) => {};
@@ -6,20 +8,39 @@ const loginUser = async (req, res) => {};
 //route for user register
 const registerUser = async (req, res) => {
   try {
-    
-    const {name,email,password} = req.body
+    const { name, email, password } = req.body;
 
-    //checking user already exists or not 
-    const exists = await userModel.findOne({email});
+    //checking user already exists or not
+    const exists = await userModel.findOne({ email });
     if (exists) {
-        return res.json({success:false,message:"user already exists"})
+      return res.json({ success: false, message: "user already exists" });
     }
 
-  } catch (error) {
-    
-  }
-};
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "please enter a strong password",
+      });
+    }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    const user = await newUser.save();
+  } catch (error) {}
+};
 //route for admin login
 const adminLogin = async (req, res) => {};
 
