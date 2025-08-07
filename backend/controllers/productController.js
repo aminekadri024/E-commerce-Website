d7ee8fbddc92c7915f6ba0b4906828faab5f502e
oussama <ademusmh166@gmail.com>
2025-08-07 21:23:58 +0100

    modified backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,20 +2,19 @@ const addProduct = async (req, res) => {
     try {
         const {name, description, price, category, subCategory, size, bestseller} = req.body;
         
-        const image1 = req.file.image1[0];
-        const image2 = req.file.image2[0];
-        const image3 = req.file.image3[0];
-        const image4 = req.file.image4[0];
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
 
         console.log(image1, image2, image3, image4);
         console.log(name, description, price, category, subCategory, size, bestseller);
         
         
-        
+        res.json({})
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message });
-        
     }
 };
 
