7c8692a507cc6801ff38dad95508d18d13b6745c
ayub-lm <ayoublamara52@gmail.com>
2025-08-30 15:53:05 +0100

    modified sidbar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -6,15 +6,15 @@ const Sidebar = () => {
     const [isActive, setIsActive] = useState()
     return (
         <div className="flex flex-col gap-y-6 p-4 border-r border-gray-300 h-screen w-40">
-            <NavLink onClick={() => setIsActive(true)} className={`${isActive ? 'border-blue-700 bg-blue-500/30' : ''} flex gap-x-1 items-center py-1 px-2 border rounded-sm`} to="/">
+            <NavLink onClick={() => setIsActive(true)} className={`${isActive ? 'border-blue-700 bg-blue-500/30' : ''} flex gap-x-1 items-center py-1 px-2 border rounded-sm`} to="/add">
                 <img src={assets.add_icon} className="size-4" />
                 <h1 className="text-sm">Add items</h1>
             </NavLink>
-            <NavLink onClick={() => setIsActive(true)} className={`${isActive ? 'border-blue-700 bg-blue-500/30' : ''} flex gap-x-1 items-center py-1 px-2 border rounded-sm`} to="/">
+            <NavLink onClick={() => setIsActive(true)} className={`${isActive ? 'border-blue-700 bg-blue-500/30' : ''} flex gap-x-1 items-center py-1 px-2 border rounded-sm`} to="/list">
                 <img src={assets.order_icon} className="size-4" />
                 <h1 className="text-sm">List Items</h1>
             </NavLink>
-            <NavLink onClick={() => setIsActive(true)} className={`${isActive ? 'border-blue-700 bg-blue-500/30' : ''} flex gap-x-1 items-center py-1 px-2 border rounded-sm`} to="/">
+            <NavLink onClick={() => setIsActive(true)} className={`${isActive ? 'border-blue-700 bg-blue-500/30' : ''} flex gap-x-1 items-center py-1 px-2 border rounded-sm`} to="/orders">
                 <img src={assets.order_icon} className="size-4" />
                 <h1 className="text-sm">Orders</h1>
             </NavLink>
